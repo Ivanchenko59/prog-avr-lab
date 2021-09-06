6b5162356b4380932e4de70c6c840500b94e1155
Ivanchenko59 <Ivanchenko59@gmail.com>
2021-09-06 22:18:22 +0300

    blinky led on paired value, button reset the counter

---
+++ lab1/Task1/Task1.ino
@@ -0,0 +1,36 @@
+#define DELAY_TIME 100
+
+int count = 0, new_time = 0;
+
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(7, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println(count);
+  
+  if(millis() - new_time > DELAY_TIME) {
+    count++;
+    new_time = millis();
+  }
+  if (digitalRead(7)) {
+    count = 0;
+  }
+  
+  if (!(count % 2)) {
+    digitalWrite(13, HIGH);
+  }
+  else {
+    digitalWrite(13, LOW);
+  }
+}
+
+/**
+ * 1. 2^16 - 1 / 2 = 32767
+ * 2. 32767
+ * 3. overflow and upcount from -32768 to 32767
+ * 4. see code
+ * 5. see code
+ */

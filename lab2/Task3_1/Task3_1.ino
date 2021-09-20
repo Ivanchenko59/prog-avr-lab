c84535f07e16d81ac29c070f042ec8bbe02f5df6
Ivanchenko59 <Ivanchenko59@gmail.com>
2021-09-20 13:27:01 +0300

    Display of numbers on the 7-segment indicator through the decoder

--- lab2/Task3_1/Task3_1.ino
+++ lab2/Task3_1/Task3_1.ino
@@ -1,5 +1,5 @@
-#define DRIVER_ONES   10
-#define DRIVER_TENS   11
+#define DRIVER_ONES   8
+#define DRIVER_TENS   9
 
 void  num_to_driver(uint8_t driver, uint8_t digit);
 void display_number(uint8_t num);
@@ -7,19 +7,43 @@ void display_number(uint8_t num);
 uint8_t num = 0;
 uint32_t new_time = 0;
 
+uint8_t digitsToSegment[10] =
+{
+    0x3F,
+    0x06,
+    0x5B,
+    0x4F,
+    0x66,
+    0x6D,
+    0x7D,
+    0x07,
+    0x7F,
+    0x6F,
+};
+
+
 void setup() {
-  for (int i = 1; i <= 13; i++) {
+  // put your setup code here, to run once:
+  for (int i = 8; i < 11; i++) {
     pinMode(i, OUTPUT);  
   }
-  PORTD = 0;
+
+  for (int i = 0; i <= 6; i++) {
+    pinMode(i, OUTPUT);  
+  }
+  
 }
 
+//8 black, 9 red, 10 brown, black-right 12
+
 void loop() {
-    display_number(num);
-    if (millis() - new_time > 1000) {
-      num = (num + 1) % 60;
-      new_time = millis();
-    }
+  // put your main code here, to run repeatedly:
+
+  display_number(num);
+  if (millis() - new_time > 1000) {
+    num = (num + 1) % 60;
+    new_time = millis();
+  }
 }
 
 void display_number(uint8_t num) 
@@ -38,5 +62,5 @@ void num_to_driver(uint8_t driver, uint8_t digit)
     digitalWrite(DRIVER_TENS, HIGH);
     digitalWrite(DRIVER_ONES, HIGH);
     (driver) ? digitalWrite(DRIVER_TENS, LOW) : digitalWrite(DRIVER_ONES, LOW);
-    PORTD = (digit << 2) & (0b1111 << 2);
+    PORTD = digitsToSegment[digit];
 }

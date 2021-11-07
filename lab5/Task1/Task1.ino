59e43d947441f36fea444dcae9e142ed8d3b14d3
Ivanchenko59 <Ivanchenko59@gmail.com>
2021-11-07 22:19:12 +0200

    potentiometr value to 7-seg using decoder

--- lab5/Task1/Task1.ino
+++ lab5/Task1/Task1.ino
@@ -3,12 +3,24 @@
 #define MAX7219_Clock 13
 
 void shift(byte send_to_address, byte send_this_data);
-void init_MAX7219(void); //Setup of MAX7219 chip
+void init_MAX7219(void);
 void display(uint16_t val);
 
 uint16_t pot_val; 
 
-void setup() {
+float k = 0.2;  
+
+float expRunningAverage(float newVal) 
+{
+    static float filVal = 0;
+    filVal += (newVal - filVal) * k;
+    return filVal;
+}
+
+
+
+void setup() 
+{
 
     pinMode(MAX7219_Data_IN, OUTPUT);
     pinMode(MAX7219_Chip_Select, OUTPUT);
@@ -16,12 +28,19 @@ void setup() {
     digitalWrite(MAX7219_Clock, HIGH);
     delay(50);
 
-    init_MAX7219();  //Setup of MAX7219 chip
+    init_MAX7219();
+    for(int i = 1; i <= 8; i++) shift(i, 0x0f);
+       
 }
 
-void loop() {
-    pot_val = analogRead(A0);
-    display(pot_val);
+void loop() 
+{
+   
+       int pot_val_prev = pot_val;
+       if(abs(pot_val_prev - expRunningAverage(analogRead(A0))) > 3) pot_val = expRunningAverage(analogRead(A0));
+       
+       display(pot_val);
+       delay(30);
 }
 
 void shift(byte send_to_address, byte send_this_data)
@@ -40,11 +59,13 @@ void init_MAX7219(void) //Setup of MAX7219 chip
     shift(0x0b, 0x07); //scan limit register - display digits 0 thru 7
     shift(0x0a, 0x0f); //intensity register - max brightness
     shift(0x09, 0xff); //decode mode register - CodeB decode all digits
+    
 }
 
-void display(uint16_t val) {
-    shift(0x04, val / 1000);
-    shift(0x03, val / 100 % 10);
-    shift(0x02, val / 10 % 10);
-    shift(0x01, val % 10);  
+void display(uint16_t val) 
+{
+    (pot_val > 999 ) ?  shift(0x04, val / 1000) : shift(0x04, 0x0f);
+    (pot_val > 100 ) ?  shift(0x03, val / 100 % 10) : shift(0x03, 0x0f);
+    (pot_val > 10 ) ?  shift(0x02, val / 10 % 10) : shift(0x02, 0x0f);
+    (pot_val >= 0 ) ?  shift(0x01, val % 10) : shift(0x01, 0x0f); 
 }
